d510eb222422f3b11507e3976f9677fddcbb1039
laril-2 <laril@viidakko.fi>
2015-06-08 11:40:36 +0300

    Updated RestClientTest.php

--- tests/php/classes/RestClientTest.php
+++ tests/php/classes/RestClientTest.php
@@ -2,17 +2,16 @@
 
 require 'RestClient.php';
 
-class Mockup_RestClient extends RestClient {
+class Mockup_RestClient extends LianaTech\RestClient {
 	public function sign(array $msg) {
 		return parent::sign($msg);
-	} 
+	}
 }
 
 class RestClientTest extends PHPUnit_Framework_TestCase {
 	public function testSign() {
-		$obj = new Mockup_RestClient('99999', 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa', 'http://mailer.localdomain', '0');
+		$obj = new Mockup_RestClient('99999', 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa', 'http://mailer.localdomain', '0', 'BTLM');
 		$hmac = $obj->sign(array('foo', 'bar'));
 		$this->assertEquals($hmac, 'c82dbf3d9574513a27f6b70cd007d16fa21686b377a9c3818f005b3af32cd04f', 'message signing');
-
 	}
 }
